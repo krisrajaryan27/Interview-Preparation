cd8742171333c7f4f04bd943c6fd7e4eeb5b8710
Sunil Soni <sunilsoni@live.in>
2017-02-25 20:23:23 +0530

    hcl f2f interview on February 25,2017 Magarpatta Pune

--- hcl/Student.java
+++ hcl/Student.java
@@ -1,6 +1,5 @@
 package com.interview.hcl;
 
-
 // An immutable class
 /*@url http://www.geeksforgeeks.org/create-immutable-class-java/
  * Following are the requirements:
@@ -10,36 +9,32 @@ package com.interview.hcl;
 •	Getter method for all the variables in it
 •	No setters(To not have option to change the value of the instance variable)
  */
-public final class Student
-{
-    final String name;
-    final int regNo;
- 
-    public Student(String name,int regNo)
-    {
-        this.name=name;
-        this.regNo=regNo;
-    }
-    public String getName()
-    {
-        return name;
-    }
-    public int getRegNo()
-    {
-        return regNo;
-    }
+public final class Student {
+	final String name;
+	final int regNo;
+
+	public Student(String name, int regNo) {
+		this.name = name;
+		this.regNo = regNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getRegNo() {
+		return regNo;
+	}
 }
- 
+
 // Driver class
-class Test
-{
-    public static void main(String args[])
-    {
-        Student s = new Student("ABC", 101);
-        System.out.println(s.name);
-        System.out.println(s.regNo);
- 
-        // Uncommenting below line causes error
-        // s.regNo = 102;
-    }
+class Test {
+	public static void main(String args[]) {
+		Student s = new Student("ABC", 101);
+		System.out.println(s.name);
+		System.out.println(s.regNo);
+
+		// Uncommenting below line causes error
+		// s.regNo = 102;
+	}
 }
--- hcl/UniqueList1.java
+++ hcl/UniqueList1.java
@@ -7,31 +7,30 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-
 //Get unique values from arraylist in java
 //Create an Arraylist of unique values
 
-
 public class UniqueList1 {
 	public static void main(String[] args) {
-			List<String> gasList = new ArrayList<>();// create list with duplicates...
-			Set<String> uniqueGas = new HashSet<String>(gasList);
-			System.out.println("Unique gas count: " + uniqueGas.size());
-			
-			//Using Java 8 Stream.
-			// Create the list with duplicates.
-			List<String> listAll = Arrays.asList("CO2", "CH4", "SO2", "CO2", "CH4", "SO2", "CO2", "CH4", "SO2");
+		List<String> gasList = new ArrayList<>();// create list with
+													// duplicates...
+		Set<String> uniqueGas = new HashSet<String>(gasList);
+		System.out.println("Unique gas count: " + uniqueGas.size());
 
-			// Create a list with the distinct elements using stream.
-			List<String> listDistinct = listAll.stream().distinct().collect(Collectors.toList());
+		// Using Java 8 Stream.
+		// Create the list with duplicates.
+		List<String> listAll = Arrays.asList("CO2", "CH4", "SO2", "CO2", "CH4", "SO2", "CO2", "CH4", "SO2");
 
-			// Display them to terminal using stream::collect with a build in Collector.
-			String collectAll = listAll.stream().collect(Collectors.joining(", "));
-			System.out.println(collectAll);
-			
-			String collectDistinct = listDistinct.stream().collect(Collectors.joining(", "));
-			System.out.println(collectDistinct);
+		// Create a list with the distinct elements using stream.
+		List<String> listDistinct = listAll.stream().distinct().collect(Collectors.toList());
 
+		// Display them to terminal using stream::collect with a build in
+		// Collector.
+		String collectAll = listAll.stream().collect(Collectors.joining(", "));
+		System.out.println(collectAll);
+
+		String collectDistinct = listDistinct.stream().collect(Collectors.joining(", "));
+		System.out.println(collectDistinct);
 
 	}
 }
